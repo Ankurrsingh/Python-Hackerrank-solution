d0d3b76029f41b58eea633bac963d7014c7fa427
Ankurrsingh <59771361+Ankurrsingh@users.noreply.github.com>
2020-06-07 11:42:20 +0530

    Create Print Function hackerrank solution.py

---
+++ Print Function hackerrank solution.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,n+1):
+        print(i,end ="")  #in python 3 print() print statements in new line and output is required in same line  thats why end is used
+        #end="" means that print()will print values without any space like 123, if we write end=" " then it will leave 1 space then print values like 1 2 3 
